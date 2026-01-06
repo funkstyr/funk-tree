7b1653685855c69caa336b9fa2f099062c97cd1b
funkstyr <funkstyr@gmail.com>
2026-01-06 15:34:16 -0800

    chore: mv old app to legacy

--- web/start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Funk Family Tree Visualization Server...
-echo.
-echo Open http://localhost:8000 in your browser
-echo Press Ctrl+C to stop the server
-echo.
-cd /d "%~dp0"
-python serve.py
-pause
